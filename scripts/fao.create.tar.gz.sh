9221938abc3b80826d1ac8522ac255d0b8e5053d
Sarven Capadisli <info@csarven.ca>
2013-03-02 09:34:09 +0100

    Minor

--- scripts/fao.create.tar.gz.sh
+++ scripts/fao.create.tar.gz.sh
@@ -3,9 +3,9 @@
 . ./fao.config.sh
 
 cd "$data"
-time tar -cvzf meta.tar.gz *DATASTRUCTURE*.rdf CL_*.rdf FAOSTAT*.rdf GENERAL_CONCEPT_SCHEME*.rdf prov.retrieval.rdf
+time tar -cvzf meta.tar.gz *DATASTRUCTURE*.rdf CL_*.rdf FAOSTAT*.rdf GENERAL_CONCEPT_SCHEME*.rdf fao.*
 
-time tar -cvzf data.tar.gz *.rdf --exclude='DATASTRUCTURE*.rdf' --exclude='prov.retrieval.rdf' --exclude='CL_*.rdf' --exclude='FAOSTAT*.rdf' --exclude='GENERAL_CONCEPT_SCHEME*.rdf' --exclude='fao.*'
+time tar -cvzf data.tar.gz *.rdf --exclude='*DATASTRUCTURE*.rdf' --exclude='CL_*.rdf' --exclude='FAOSTAT*.rdf' --exclude='GENERAL_CONCEPT_SCHEME*.rdf' --exclude='fao.*'
 
 #326M    data.tar.gz
 #1.9M    meta.tar.gz
