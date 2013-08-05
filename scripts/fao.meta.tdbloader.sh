60fd20806ddde9d20f94ffef3c2d586a032034a1
Sarven Capadisli <info@csarven.ca>
2013-08-06 00:13:46 +0200

    Added dcterms

--- scripts/fao.meta.tdbloader.sh
+++ scripts/fao.meta.tdbloader.sh
@@ -7,6 +7,7 @@
 
 . ./fao.config.sh
 
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$graphs"dcterms.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$graphs"void.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$graphs"extra.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$graphs"rdf.nt
